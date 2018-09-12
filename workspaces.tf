7b9d70203682d6b85bf47607e4e4a428fe1e78b7
Quackers <24840026+Quackers71@users.noreply.github.com>
2018-09-12 08:57:50 +0100

    Workspaces - exercise 25

--- workspaces.tf
+++ workspaces.tf
@@ -1,22 +1,22 @@
+
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
-terraform {
-  resource "aws_s3_bucket" {
-    bucket = "terraform-demo-q2018"
-    key    = "network/terraform.tfstate"
-	region = "us-east-1"
-  }
-}
-
 provider "aws" {
-	
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "us-east-1"
+  region = "eu-west-2"
+}
+
+locals {
+#  default_name = "${join("-", list(terraform.workspace, "example"))}"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0ff8a91507f77f867"
+  tags = {
+    Name = "${local.default_name}"
+  }
+
+  ami           = "ami-f976839e"
   instance_type = "t2.micro"
 }
